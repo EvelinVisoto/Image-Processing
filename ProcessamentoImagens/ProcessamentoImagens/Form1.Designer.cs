54f46a693909395784855503d83edc192e41c6d1
Evelin Visoto <128976859+EvelinVisoto@users.noreply.github.com>
2025-03-29 20:16:09 -0300

    Add files via upload

---
+++ ProcessamentoImagens/ProcessamentoImagens/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace ProcessamentoImagens
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.processButton = new System.Windows.Forms.Button();
+            this.pbOriginal = new System.Windows.Forms.PictureBox();
+            this.pbGray = new System.Windows.Forms.PictureBox();
+            this.pbBrilho = new System.Windows.Forms.PictureBox();
+            this.pbBinaria = new System.Windows.Forms.PictureBox();
+            this.pbRotacionada = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGray)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBrilho)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBinaria)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRotacionada)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // processButton
+            // 
+            this.processButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.processButton.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.processButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.processButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.processButton.FlatAppearance.BorderSize = 0;
+            this.processButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.processButton.Location = new System.Drawing.Point(482, 32);
+            this.processButton.Name = "processButton";
+            this.processButton.Size = new System.Drawing.Size(138, 29);
+            this.processButton.TabIndex = 0;
+            this.processButton.Text = "Processar Imagens";
+            this.processButton.UseVisualStyleBackColor = false;
+            this.processButton.Click += new System.EventHandler(this.processButton_Click_1);
+            // 
+            // pbOriginal
+            // 
+            this.pbOriginal.Location = new System.Drawing.Point(12, 100);
+            this.pbOriginal.Name = "pbOriginal";
+            this.pbOriginal.Size = new System.Drawing.Size(322, 241);
+            this.pbOriginal.TabIndex = 1;
+            this.pbOriginal.TabStop = false;
+            // 
+            // pbGray
+            // 
+            this.pbGray.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbGray.Location = new System.Drawing.Point(749, 100);
+            this.pbGray.Name = "pbGray";
+            this.pbGray.Size = new System.Drawing.Size(322, 241);
+            this.pbGray.TabIndex = 2;
+            this.pbGray.TabStop = false;
+            // 
+            // pbBrilho
+            // 
+            this.pbBrilho.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pbBrilho.Location = new System.Drawing.Point(386, 100);
+            this.pbBrilho.Name = "pbBrilho";
+            this.pbBrilho.Size = new System.Drawing.Size(322, 241);
+            this.pbBrilho.TabIndex = 3;
+            this.pbBrilho.TabStop = false;
+            // 
+            // pbBinaria
+            // 
+            this.pbBinaria.Location = new System.Drawing.Point(217, 368);
+            this.pbBinaria.Name = "pbBinaria";
+            this.pbBinaria.Size = new System.Drawing.Size(322, 250);
+            this.pbBinaria.TabIndex = 4;
+            this.pbBinaria.TabStop = false;
+            // 
+            // pbRotacionada
+            // 
+            this.pbRotacionada.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbRotacionada.Location = new System.Drawing.Point(567, 368);
+            this.pbRotacionada.Name = "pbRotacionada";
+            this.pbRotacionada.Size = new System.Drawing.Size(322, 295);
+            this.pbRotacionada.TabIndex = 5;
+            this.pbRotacionada.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(1083, 680);
+            this.Controls.Add(this.pbRotacionada);
+            this.Controls.Add(this.pbBinaria);
+            this.Controls.Add(this.pbBrilho);
+            this.Controls.Add(this.pbGray);
+            this.Controls.Add(this.pbOriginal);
+            this.Controls.Add(this.processButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pbOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGray)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBrilho)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBinaria)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRotacionada)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button processButton;
+        private System.Windows.Forms.PictureBox pbOriginal;
+        private System.Windows.Forms.PictureBox pbGray;
+        private System.Windows.Forms.PictureBox pbBrilho;
+        private System.Windows.Forms.PictureBox pbBinaria;
+        private System.Windows.Forms.PictureBox pbRotacionada;
+    }
+}
+
---
+++ ProcessamentoImagens/ProcessamentoImagens/Form1.cs
@@ -0,0 +1,220 @@
+﻿/* Colegio Técnico Antônio Teixeira Fernandes (Univap)
+ * Curso Técnico em Informática - Data de Entrega: 01 / 09 / 2024
+ * Autores do Projeto: Evelin Visoto Carneiro Fernandes
+ *
+ * Turma: 3F
+ * Atividade Proposta em aula
+ * Observação: < Projeto do terceiro bimestre >
+ * ******************************************************************/
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ProcessamentoImagens
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void processButton_Click_1(object sender, EventArgs e)
+        {
+            Bitmap imgA = new Bitmap(@"C:\Users\inais\Pictures\projetoICG\Im.jpg");
+            Bitmap imgB = new Bitmap(@"C:\Users\inais\Pictures\projetoICG\Balao.jpg");
+            Bitmap imgC = new Bitmap(@"C:\Users\inais\Pictures\projetoICG\Aviao2.jpg");
+            Bitmap imgD = new Bitmap(@"C:\Users\inais\Pictures\projetoICG\homem.jpg");
+
+            imgB = RemoverFundo(imgB, Color.FromArgb(83, 144, 203));
+            imgC = RemoverFundo(imgC, Color.FromArgb(64, 146, 214));
+            imgD = RemoverFundo(imgD, Color.FromArgb(166, 144, 107));
+
+            Bitmap imgE = CombinarImagens(imgA, imgB, imgC, imgD);
+
+            Bitmap imgBrilho = AumentarBrilho(new Bitmap(imgE), 85);
+            Bitmap imgGray = ConverterParaGrayLevel(new Bitmap(imgE));
+            Bitmap imgBinaria = ConverterParaBinario(imgGray, 126);
+            Bitmap imgRotacionada = RotacionarImagem(new Bitmap(imgE), 90);
+
+            imgE.Save(@"C:\Users\inais\Pictures\imgE_Combinada.jpg");
+            imgBrilho.Save(@"C:\Users\inais\Pictures\imgE_Brilho.jpg");
+            imgGray.Save(@"C:\Users\inais\Pictures\imgE_Gray.jpg");
+            imgBinaria.Save(@"C:\Users\inais\Pictures\imgE_Binaria.jpg");
+            imgRotacionada.Save(@"C:\Users\inais\Pictures\imgE_Rotacionada.jpg");
+
+            ExibirImagens(imgE, imgBrilho, imgGray, imgBinaria, imgRotacionada);
+        }
+
+        private Bitmap RemoverFundo(Bitmap imagem, Color corFundo)
+        {
+            Bitmap imagemSemFundo = new Bitmap(imagem.Width, imagem.Height);
+
+            for (int i = 0; i < imagem.Width; i++)
+            {
+                for (int j = 0; j < imagem.Height; j++)
+                {
+                    Color pixel = imagem.GetPixel(i, j);
+
+                    if ((pixel.R >= corFundo.R - 20 && pixel.R <= corFundo.R + 20) &&
+                        (pixel.G >= corFundo.G - 20 && pixel.G <= corFundo.G + 20) &&
+                        (pixel.B >= corFundo.B - 20 && pixel.B <= corFundo.B + 20))
+                    {
+                        imagemSemFundo.SetPixel(i, j, Color.FromArgb(0, 0, 0, 0));
+                    }
+                    else
+                    {
+                        imagemSemFundo.SetPixel(i, j, pixel);
+                    }
+                }
+            }
+
+            return imagemSemFundo;
+        }
+
+
+        private Bitmap CombinarImagens(Bitmap imagemBase, Bitmap imagemSB, Bitmap imagemSC, Bitmap imagemSD)
+        {
+            Bitmap imagemFinal = new Bitmap(imagemBase);
+
+            imagemFinal = InserirImagem(imagemFinal, imagemSB, 500, 100);
+            imagemFinal = InserirImagem(imagemFinal, imagemSC, 100, 60);
+            imagemFinal = InserirImagem(imagemFinal, imagemSD, 300, 500);
+
+            return imagemFinal;
+        }
+
+        private Bitmap InserirImagem(Bitmap imagemBase, Bitmap imagemSobreposta, int x, int y)
+        {
+            Bitmap imagemE = new Bitmap(imagemBase);
+
+            for (int i = 0; i < imagemSobreposta.Width; i++)
+            {
+                for (int j = 0; j < imagemSobreposta.Height; j++)
+                {
+                    Color pixel = imagemSobreposta.GetPixel(i, j);
+                    if (pixel.A > 0)
+                    {
+                        imagemE.SetPixel(i + x, j + y, pixel);
+                    }
+                }
+            }
+            return imagemE;
+        }
+
+        private Bitmap AumentarBrilho(Bitmap img, int brilho)
+        {
+            for (int y = 0; y < img.Height; y++)
+            {
+                for (int x = 0; x < img.Width; x++)
+                {
+                    Color pixel = img.GetPixel(x, y);
+                    int r = Math.Min(Math.Max(pixel.R + brilho, 0), 255);
+                    int g = Math.Min(Math.Max(pixel.G + brilho, 0), 255);
+                    int b = Math.Min(Math.Max(pixel.B + brilho, 0), 255);
+                    img.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+            }
+            return img;
+        }
+
+        private Bitmap ConverterParaGrayLevel(Bitmap img)
+        {
+            Bitmap grayImg = new Bitmap(img.Width, img.Height);
+            for (int y = 0; y < img.Height; y++)
+            {
+                for (int x = 0; x < img.Width; x++)
+                {
+                    Color pixel = img.GetPixel(x, y);
+                    int gray = (int)(pixel.R * 0.30 + pixel.G * 0.59 + pixel.B * 0.11);
+                    grayImg.SetPixel(x, y, Color.FromArgb(gray, gray, gray));
+                }
+            }
+            return grayImg;
+        }
+
+        private Bitmap ConverterParaBinario(Bitmap img, int threshold)
+        {
+            Bitmap binariaImg = new Bitmap(img.Width, img.Height);
+            for (int y = 0; y < img.Height; y++)
+            {
+                for (int x = 0; x < img.Width; x++)
+                {
+                    Color pixel = img.GetPixel(x, y);
+                    int gray = pixel.R;
+                    int bin = gray <= threshold ? 0 : 255;
+                    binariaImg.SetPixel(x, y, Color.FromArgb(bin, bin, bin));
+                }
+            }
+            return binariaImg;
+        }
+
+        private Bitmap RotacionarImagem(Bitmap img, float angulo)
+        {
+            double radian = angulo * Math.PI / 180;
+
+            int novaLargura = (int)(img.Width * Math.Abs(Math.Cos(radian)) + img.Height * Math.Abs(Math.Sin(radian)));
+            int novaAltura = (int)(img.Width * Math.Abs(Math.Sin(radian)) + img.Height * Math.Abs(Math.Cos(radian)));
+
+            Bitmap rotatedImg = new Bitmap(novaLargura, novaAltura);
+
+            int centroXOriginal = img.Width / 2;
+            int centroYOriginal = img.Height / 2;
+            int centroXRotacionada = novaLargura / 2;
+            int centroYRotacionada = novaAltura / 2;
+
+            for (int x = 0; x < novaLargura; x++)
+            {
+                for (int y = 0; y < novaAltura; y++)
+                {
+                    int xOriginal = (int)((x - centroXRotacionada) * Math.Cos(-radian) - (y - centroYRotacionada) * Math.Sin(-radian) + centroXOriginal);
+                    int yOriginal = (int)((x - centroXRotacionada) * Math.Sin(-radian) + (y - centroYRotacionada) * Math.Cos(-radian) + centroYOriginal);
+
+                    if (xOriginal >= 0 && xOriginal < img.Width && yOriginal >= 0 && yOriginal < img.Height)
+                    {
+                        Color pixel = img.GetPixel(xOriginal, yOriginal);
+                        rotatedImg.SetPixel(x, y, pixel);
+                    }
+                    else
+                    {
+                        rotatedImg.SetPixel(x, y, Color.FromArgb(0, 0, 0, 0));
+                    }
+                }
+            }
+
+            return rotatedImg;
+        }
+
+
+
+        private void ExibirImagens(Bitmap imgOriginal, Bitmap imgBrilho, Bitmap imgGray, Bitmap imgBinaria, Bitmap imgRotacionada)
+        {
+            pbOriginal.Image = imgOriginal;
+            pbOriginal.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            pbBrilho.Image = imgBrilho;
+            pbBrilho.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            pbGray.Image = imgGray;
+            pbGray.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            pbBinaria.Image = imgBinaria;
+            pbBinaria.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            pbRotacionada.Image = imgRotacionada;
+            pbRotacionada.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+       
+    }
+}
